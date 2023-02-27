940f44c75568b70c2939e5da8be59abd50bb70c9
JayMon0327 <kioplm0211@naver.com>
2023-02-27 14:46:38 +0900

    Commit2 나누기 기능 제거 제곱 기능 추가

--- first.py
+++ first.py
@@ -1,45 +1,22 @@
 ## 메서드 선언부
-def add_func( n1, n2 ):
-    res = n1/n2
+#def div_func( n1, n2 ):
+#    res = n1/n2
+#    return res
+
+def mul_func( n1, n2 ):
+    res = n1*n2
     return res
 
-##전역변수 (인스턴스 변수, 클래스변수)
-num1,num2,result = 100,200,0
-
-##메인 코드부 ( main() 메서드 )
-##result = num1 + num2
-result = add_func(num1,num2)
-print(num1,'/',num2,'=',result)
-
-## 메서드 선언부
 def add_func( n1, n2 ):
     res = n1+n2
     return res
 
-##전역변수 (인스턴스 변수, 클래스변수)
-num1,num2,result = 100,200,0
-
-##메인 코드부 ( main() 메서드 )
-##result = num1 + num2
-result = add_func(num1,num2)
-print(num1,'+',num2,'=',result)
-
-## 메서드 선언부
-def add_func( n1, n2 ):
+def min_func( n1, n2 ):
     res = n1-n2
     return res
 
-##전역변수 (인스턴스 변수, 클래스변수)
-num1,num2,result = 100,200,0
-
-##메인 코드부 ( main() 메서드 )
-##result = num1 + num2
-result = add_func(num1,num2)
-print(num1,'-',num2,'=',result)
-
-## 메서드 선언부
-def add_func( n1, n2 ):
-    res = n1*n2
+def double_func( n1, n2 ):
+    res = n1**n2
     return res
 
 ##전역변수 (인스턴스 변수, 클래스변수)
@@ -47,5 +24,17 @@ num1,num2,result = 100,200,0
 
 ##메인 코드부 ( main() 메서드 )
 ##result = num1 + num2
+#result = div_func(num1,num2)
+#print(num1,'/',num2,'=',result)
+
 result = add_func(num1,num2)
 print(num1,'*',num2,'=',result)
+
+result = double_func(num1,num2)
+print(num1,'**',num2,'=',result)
+
+result = add_func(num1,num2)
+print(num1,'+',num2,'=',result)
+
+result = add_func(num1,num2)
+print(num1,'-',num2,'=',result)
