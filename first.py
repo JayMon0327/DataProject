860ed845d7b4d90fd02df2af53617bb06699a3d7
JayMon0327 <kioplm0211@naver.com>
2023-02-27 14:39:45 +0900

    commit 1 첫번째 코드를 커밋함.

---
+++ first.py
@@ -0,0 +1,51 @@
+## 메서드 선언부
+def add_func( n1, n2 ):
+    res = n1/n2
+    return res
+
+##전역변수 (인스턴스 변수, 클래스변수)
+num1,num2,result = 100,200,0
+
+##메인 코드부 ( main() 메서드 )
+##result = num1 + num2
+result = add_func(num1,num2)
+print(num1,'/',num2,'=',result)
+
+## 메서드 선언부
+def add_func( n1, n2 ):
+    res = n1+n2
+    return res
+
+##전역변수 (인스턴스 변수, 클래스변수)
+num1,num2,result = 100,200,0
+
+##메인 코드부 ( main() 메서드 )
+##result = num1 + num2
+result = add_func(num1,num2)
+print(num1,'+',num2,'=',result)
+
+## 메서드 선언부
+def add_func( n1, n2 ):
+    res = n1-n2
+    return res
+
+##전역변수 (인스턴스 변수, 클래스변수)
+num1,num2,result = 100,200,0
+
+##메인 코드부 ( main() 메서드 )
+##result = num1 + num2
+result = add_func(num1,num2)
+print(num1,'-',num2,'=',result)
+
+## 메서드 선언부
+def add_func( n1, n2 ):
+    res = n1*n2
+    return res
+
+##전역변수 (인스턴스 변수, 클래스변수)
+num1,num2,result = 100,200,0
+
+##메인 코드부 ( main() 메서드 )
+##result = num1 + num2
+result = add_func(num1,num2)
+print(num1,'*',num2,'=',result)
